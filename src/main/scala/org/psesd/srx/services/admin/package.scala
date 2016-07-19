ab9f0e2c1761a0a5bcac3e23187ee4f4b54e586d
Stephen <stephen.pugmire@itrellis.com>
2016-07-19 15:32:52 -0700

    Added build info.

---
+++ src/main/scala/org/psesd/srx/services/admin/package.scala
@@ -0,0 +1,11 @@
+package org.psesd.srx.services
+
+/** Provides administrative Student Record Exchange services shared by SRX components and services.
+  *
+  * @version 1.0
+  * @since 1.0
+  * @author Stephen Pugmire (iTrellis, LLC)
+  */
+package object admin {
+
+}
